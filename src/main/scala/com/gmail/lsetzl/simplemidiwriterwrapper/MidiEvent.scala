c5b5cd2ae109c59a783aef4a960deeb06efb8168
Setz <lsetzl@gmail.com>
2019-05-30 16:57:19 +0900

    add MidiEvents

--- src/main/scala/com/gmail/lsetzl/simplemidiwriterwrapper/MidiEvent.scala
+++ src/main/scala/com/gmail/lsetzl/simplemidiwriterwrapper/MidiEvent.scala
@@ -15,4 +15,31 @@ object MidiEvent {
     override val midiMessage: MidiMessage = MetaMessage.Tempo(value)
   }
 
+  case class NoteOff(channel: Int, key: Int, override val tick: Long) extends MidiEvent {
+    override val midiMessage: MidiMessage = ShortMessage.NoteOff(channel, key)
+  }
+
+  case class NoteOn(channel: Int, key: Int, velocity: Int, override val tick: Long) extends MidiEvent {
+    override val midiMessage: MidiMessage = ShortMessage.NoteOn(channel, key, velocity)
+  }
+
+  case class PolyPressure(channel: Int, key: Int, pressure: Int, override val tick: Long) extends MidiEvent {
+    override val midiMessage: MidiMessage = ShortMessage.PolyPressure(channel, key, pressure)
+  }
+
+  case class ControlChange(channel: Int, number: Int, value: Int, override val tick: Long) extends MidiEvent {
+    override val midiMessage: MidiMessage = ShortMessage.ControlChange(channel, number, value)
+  }
+
+  case class ProgramChange(channel: Int, value: Int, override val tick: Long) extends MidiEvent {
+    override val midiMessage: MidiMessage = ShortMessage.ProgramChange(channel, value)
+  }
+
+  case class ChannelPressure(channel: Int, pressure: Int, override val tick: Long) extends MidiEvent {
+    override val midiMessage: MidiMessage = ShortMessage.ChannelPressure(channel, pressure)
+  }
+
+  case class PitchBend(channel: Int, value: Int, override val tick: Long) extends MidiEvent {
+    override val midiMessage: MidiMessage = ShortMessage.PitchBend(channel, value)
+  }
 }
